bd72c75011bcfb1ea86701535a78a5aa6047cbac
OliverioJunior <oliverio.junior2@gmail.com>
2025-03-11 17:05:40 -0300

    feat: criar component header da melhor forma

---
+++ src/components/ui/header/Header.tsx
@@ -0,0 +1,11 @@
+import React, { memo } from "react";
+import { HeaderProps } from "./types";
+import "./Header.styles.css";
+
+export const Header = memo<HeaderProps>(({ children, className = "" }) => {
+  const headerClassName = `header ${className}`.trim();
+
+  return <header className={headerClassName}>{children}</header>;
+});
+
+Header.displayName = "Header";
