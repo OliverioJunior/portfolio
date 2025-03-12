3aee18089900c2c93c737a2a683be7e906d08826
OliverioJunior <oliverio.junior2@gmail.com>
2025-03-12 18:27:33 -0300

    feat: chamar componentes na home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,61 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
-
+import { Header, Navbar, NavItem } from "@/components";
+import { FaHome, FaBlog } from "react-icons/fa";
+import { GrProjects } from "react-icons/gr";
+import { MdOutlineWork } from "react-icons/md";
 export default function Home() {
+  const navItems = [
+    {
+      label: "Home",
+      href: "/",
+      isActive: true,
+      icon: <FaHome />,
+    },
+    {
+      label: "Experiências",
+      href: "/experiences",
+      icon: <MdOutlineWork />,
+    },
+    {
+      label: "Projects",
+      href: "/projects",
+      icon: <GrProjects />,
+      children: [
+        { label: "Web", href: "/projects/web" },
+        { label: "Mobile", href: "/projects/mobile" },
+      ],
+    },
+    {
+      label: "Blog",
+      href: "/blog",
+      icon: <FaBlog />,
+    },
+  ];
+
+  const handleNavItemClick = (item: NavItem) => {
+    console.log("Clicked:", item.label);
+  };
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+      <Header>
+        <Navbar
+          items={navItems}
+          logo={
             <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              width={50}
+              height={50}
+              src="/Sabedoria.svg"
+              alt="Logo"
+              priority
             />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          }
+          variant="colored"
+          position="sticky"
+          onNavItemClick={handleNavItemClick}
+        />
+      </Header>
     </div>
   );
 }
