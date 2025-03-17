5035c90d20a5653f6af82dc31aaccfee5634758e
OliverioJunior <oliverio.junior2@gmail.com>
2025-03-16 22:46:26 -0300

    feat: criar component `CustomCursor`

---
+++ src/modules/portfolio/ui/custom-cursor/CustomCursor.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { useEffect, useState } from "react";
+import styles from "./CustomCursor.module.css";
+
+export function CustomCursor() {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const updateCursorPosition = (e: MouseEvent) => {
+      setPosition({ x: e.clientX, y: e.clientY });
+    };
+
+    window.addEventListener("mousemove", updateCursorPosition);
+
+    return () => {
+      window.removeEventListener("mousemove", updateCursorPosition);
+    };
+  }, []);
+
+  return (
+    <div
+      className={styles.cursorGlow}
+      style={{
+        left: `${position.x}px`,
+        top: `${position.y}px`,
+      }}
+    />
+  );
+}
